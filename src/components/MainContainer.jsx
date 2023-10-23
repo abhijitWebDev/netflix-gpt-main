3361e109f64162f943c76c86153ffafb5354bdbb
abhijitWebDev <110275876+abhijitWebDev@users.noreply.github.com>
2023-10-23 15:41:29 +0530

    fetching data in title and overview in video title

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import VideoTitle from './VideoTitle';
+import VideoBackground from './VideoBackground';
 
 const MainContainer = () => {
+    const movies = useSelector((state) => state.movies ?. nowPlayingMovies);
+    if(!movies) return ;
+    const oneMainMovie = movies[1];
+
+    console.log(oneMainMovie)
+    const{original_title, overview} = oneMainMovie;
   return (
-    <div>MainContainer</div>
+    <>
+    <VideoTitle title={original_title} overview={overview} />
+    <VideoBackground />
+    </>
   )
 }
 
